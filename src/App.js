9727d13d9d2120e40fd419a0037d1d985e48111f
Sidit Srivastava <siditsrivastava84@gmail.com>
2024-02-14 12:47:16 +0530

    ruby

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Page from './comp/Page/Page';
+
+function App() {
+  return (
+   <>
+    <Page/>
+   </>
+  );
+}
+
+export default App;
